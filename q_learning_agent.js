b87b3ac19450948b770ad9ebaf6048dd352dc2b9
izabayop <izabayop@wcc02622687.its.carleton.edu>
2024-11-03 17:31:19 -0600

    Added the q-learning agent

---
+++ q_learning_agent.js
@@ -0,0 +1,46 @@
+
+
+class QLearningAgent {
+    constructor(actions) {
+        this.qTable = {};
+        this.actions = actions;
+        this.alpha = 0.2; // Learning rate
+        this.gamma = 0.95; // Discount factor
+        this.epsilon = 1.0; // Exploration rate
+    }
+
+    getQValue(state, action) {
+        return this.qTable[`${state}:${action}`] || 0.0;
+    }
+
+    updateQValue(state, action, reward, nextState) {
+        const bestNextQ = Math.max(...this.actions.map(a => this.getQValue(nextState, a)));
+        this.qTable[`${state}:${action}`] = this.getQValue(state, action) +
+            this.alpha * (reward + this.gamma * bestNextQ - this.getQValue(state, action));
+    }
+
+    chooseAction(state) {
+        // if (Math.random() < this.epsilon) {
+        //     return this.actions[Math.floor(Math.random() * this.actions.length)]; // Explore
+        // } else {
+            return this.actions.reduce((bestAction, a) => 
+                this.getQValue(state, a) > this.getQValue(state, bestAction) ? a : bestAction
+            ); // Exploit
+        
+    }
+}
+
+// agent initialization
+const agent = new QLearningAgent([0, 1]); // 0 = do nothing, 1 = buy property
+
+// Training loop
+for (let episode = 0; episode < 1000; episode++) { // Run multiple episodes
+    let state = game.reset();
+    let done = false;
+    while (!done) {
+        const action = agent.chooseAction(state);
+        const [nextState, reward] = game.step(action);
+        agent.updateQValue(state, action, reward, nextState);
+        state = nextState;
+    }
+}
