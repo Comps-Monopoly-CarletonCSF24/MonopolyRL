06b140b5a523e8523165b7b8438434c67acd6fe9
Dake Peng <pengd@carleton.edu>
2024-11-09 23:49:25 -0600

    added tests

--- classes/state.py
+++ classes/state.py
@@ -1,120 +1,136 @@
-from player import Player
-from board import Board, Property, Cell
-from log import Log
+from classes.player import Player
+from classes.board import Board, Property, Cell
+from classes.log import Log
 import numpy as np
 
-property_by_group_zeros = {'Brown': 0, 'Railroads': 0, 'Lightblue':0, 
-                        'Pink': 0, 'Utilities': 0, 'Orange': 0,
-                        'Red': 0, 'Yellow': 0, 'Green': 0, 'Indigo': 0}
-num_property_per_group = {'Brown': 2, 'Railroads': 4, 'Lightblue': 3, 
-                        'Pink': 3, 'Utilities': 4, 'Orange': 3,
-                        'Red': 3, 'Yellow': 3, 'Green': 3, 'Indigo': 2}
-group_indices = {'Brown': 0, 'Railroads': 1, 'Lightblue': 2, 'Pink': 3, 
-                    'Utilities': 4, 'Orange': 5, 'Red': 6, 'Yellow': 7, 
-                    'Green': 8, 'Indigo': 9}
+# the number of properties on the board in each group
+num_property_per_group = {'Brown': 2, 'Railroads': 4, 'Lightblue': 3, 'Pink': 3, 'Utilities': 4, 'Orange': 3, 'Red': 3, 'Yellow': 3, 'Green': 3, 'Indigo': 2}
+# tn arbitrary index for groups between 1 - 9
+group_indices = {'Brown': 0, 'Railroads': 1, 'Lightblue': 2, 'Pink': 3, 'Utilities': 4, 'Orange': 5, 'Red': 6, 'Yellow': 7, 'Green': 8, 'Indigo': 9}
+# number of groups ob the board
 Num_Groups = 10
-#least common multiple
+# least common multiple for the number of property per group on the board
 LCM_Property_Per_Group = 12
+# number of cells on the board
 Num_Total_Cells = 40
-
+# a number to represent how much property the player owns within one color, max 17
+Total_Property_Points = 17
 class State:
     state = None
-    def __init__(self, current_player: Player, players: list, board):
-        area = self.get_area(current_player, players)
-        position = self.get_position(current_player.position)
-        finance = self.get_finance(current_player, players)
-        self.state = self.get_state(area, position, finance)
-
-    def get_area(self, current_player: Player, players: Player) -> np.ndarray:
-        """Reads the board for properties belonging to the agent and other players.
-
-        Args:
-            board (Board): _description_
-
-        Returns:
-            np.ndarray: _description_
-        """
-        self_property_by_group = self.get_property_by_group(current_player)
-        others_property_by_group = np.zeros(Num_Groups)
-        for player in players:
-            if not (player.is_bankrupt or player.name == current_player.name):
-                others_property_by_group += self.get_property_by_group(player)
-        area = np.vstack(self_property_by_group, others_property_by_group)
-        self.area = area
-    
-    def get_property_by_group(player:Player) -> np.ndarray:
-        """Gets the number of property of each group that a player has
-
-        Args:
-            player (Player): _description_
-
-        Returns:
-            property_by_group: _description_
-        """
-        property_by_group = [0] * Num_Groups
-        for property in player.owned:
-            group_index = group_indices[property.group]
-            property_by_group[group_index] += LCM_Property_Per_Group / num_property_per_group[property.group]
-            if property.has_houses:
-                property_by_group[group_index] = LCM_Property_Per_Group + property.has_houses
-            if property.has_hotel:
-                property_by_group[group_index] = LCM_Property_Per_Group + property.has_hotel
-        return np.array(property_by_group)
-    
-    def get_position(self, position_int : int) -> float:
-        """Converts a position in [0,39] to one in [0,1)
+    def __init__(self, current_player: Player, players: list):
+        area = get_area(current_player, players)
+        position = get_position(current_player.position)
+        finance = get_finance(current_player, players)
+        self.state = get_state(area, position, finance)
         
-        Args:
-            position_int (int): _description_
-        """
-        position_float = (position_int) / (Num_Total_Cells - 1)
-        self.position = position_float
-        
-    def get_finance(self, current_player: Player, players: list) -> np.ndarray:
-        """Gets the finance state vector from the player's money and properties
+def get_area(current_player: Player, players: Player) -> np.ndarray:
+    """ returns the area vector describing property owning percentage for each color
+    Args:
+        board (Board): _description_
 
-        Args:
-            money (_type_): _description_
-            properties (_type_): _description_
-        """
-        property_others_accumulated = 0
-        for player in players:
-            if not (player.is_bankrupt or player.name == current_player.name):
-                property_others_accumulated += self.get_num_property(player)
-        property_ratio = self.get_num_property(current_player) / property_others_accumulated
-        money_normalized = self.sigmoid_money(current_player.money)
-        finance = np.array([property_ratio, money_normalized])
-        self.finance = finance
+    Returns:
+        np.ndarray: each index (according to the assigned group indices) represents 
+        the percentage of property points earned in each color group
+    """
+    self_property_points = get_property_points_by_group(current_player)
+    others_property_points = np.zeros(Num_Groups)
+    for player in players:
+        if not (player.is_bankrupt or player.name == current_player.name):
+            others_property_points += get_property_points_by_group(player)
+    area = np.vstack((self_property_points, others_property_points)) / Total_Property_Points
+    return area
 
-    def get_num_property(player: Player):
-        """returns the number of properties a player has
+def get_property_points_by_group(player:Player) -> np.ndarray:
+    """Gets the number of property of each group that a player has
 
-        Args:
-            player (Player): _description_
-        """
-        total_property = 0
-        for property in player.owned:
-            total_property += 1 + property.has_hotel + property.has_houses
-        return total_property
+    Args:
+        player (Player): /
 
-    def sigmoid_money(money):
-        """normalizes the amount of money a player has with a sigmoid function
+    Returns:
+        np.ndarray: each index (according to the assigned group indices) represents 
+        property points that a player owns, max 17. For all land in the group the 
+        player gets 12 (take fractions if not all owned), and for each house on 
+        any property in that group, the player gets 1 point
+    """
+    property_by_group = [0] * Num_Groups
+    for property in player.owned:
+        group_index = group_indices[property.group]
+        property_by_group[group_index] += LCM_Property_Per_Group / num_property_per_group[property.group]
+        if property.has_hotel > 0:
+            property_by_group[group_index] = Total_Property_Points
+        elif property.has_houses > 0:
+            property_by_group[group_index] = LCM_Property_Per_Group + property.has_houses
+            
+    return np.array(property_by_group)
 
-        Args:
-            money (_type_): _description_
+def get_position(position_int : int) -> float:
+    """Converts a position in [0,39] to one in [0,1] by dividing 39
 
-        Returns:
-            _type_: _description_
-        """
-        return money / ( 1 + abs(money))
-       
-    def get_state(self) -> np.ndarray:
-        """converts the 3 vectors/integers into a new, 1*23 vector.
+    Args:
+        position_int (int): a number between 0 and 39 representing the players position
 
-        Returns:
-            np.ndarray: _description_
-        """
-        # Flatten the 2x10 area array to 1x20
-        flattened_area = self.area.flatten()
-        # Combine all into a 1x23 array
-        self.state  = np.concatenate((flattened_area, [self.position], self.finance))
+    Returns:
+        float: a number between 0 and 1 representing the players position
+    """
+    position_float = (position_int) / (Num_Total_Cells - 1)
+    return position_float
+    
+def get_finance(current_player: Player, players: list) -> np.ndarray:
+    """Gets the finance state vector from the player's money and properties
+
+    Args:
+        current_player (Player): /
+        players (list): a list of Player objects representing players that are alive
+
+    Returns:
+        np.ndarray: _description_
+    """
+
+    property_others_accumulated = 0
+    for player in players:
+        if not (player.is_bankrupt or player.name == current_player.name):
+            property_others_accumulated += get_num_property(player)
+    property_ratio = get_num_property(current_player) / property_others_accumulated
+    money_normalized = sigmoid_money(current_player.money)
+    finance = np.array([property_ratio, money_normalized])
+    return finance
+
+def get_num_property(player: Player, houses = False) -> int:
+    """returns the number of properties a player has
+
+    Args:
+        player (Player): /
+        houses (bool, optional): whether to count houses. Defaults to False.
+
+    Returns:
+        int: total number of property the player has
+    """
+    total_property = 0
+    for property in player.owned:
+        total_property += 1 
+        if houses:
+            total_property += property.has_hotel + property.has_houses
+    return total_property
+
+def sigmoid_money(money: int) -> float:
+    """normalizes the amount of money a player has with a sigmoid function
+
+    Args:
+        money (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return money / ( 1 + abs(money))
+    
+def get_state(area: np.ndarray, position: int, finance: np.ndarray) -> np.ndarray:
+    """converts the 3 vectors/integers into a new one-dimensional vector
+
+    Returns:
+        state(np.ndarray): a 1 * 23 vector representing the state
+    """
+    # Flatten the 2x10 area array to 1x20
+    flattened_area = area.flatten()
+    # Combine all into a 1x23 array
+    state = np.concatenate((flattened_area, [position], finance))
+    return state
---
+++ tests/test_basic_qlearning_agent.py
@@ -0,0 +1,87 @@
+import random
+import unittest
+import sys
+import os
+import numpy as np
+
+# add parent directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from settings import GameSettings, LogSettings
+from classes.player import Player
+from classes.board import Board
+from classes.dice import Dice
+from classes.log import Log
+from classes.state import State
+
+def init_game(game_seed):
+    
+    # Initialize game log
+    log = Log(LogSettings.game_log_file, disabled=not LogSettings.keep_game_log)
+
+    # Initialize data log
+    datalog = Log(LogSettings.data_log_file)
+
+    # Initialize the board (plots, chance, community chest etc)
+    board = Board(GameSettings)
+
+    # Set up dice (it creates a separate random generator with initial "game_seed",
+    # to have thread-safe shuffling and dice throws)
+    dice = Dice(game_seed, GameSettings.dice_count, GameSettings.dice_sides, log)
+
+    # Shuffle chance and community chest cards
+    # (using thread-safe random generator)
+    dice.shuffle(board.chance.cards)
+    dice.shuffle(board.chest.cards)
+
+    # Set up players with their behavior settings
+    players = [Player(player_name, player_setting)
+               for player_name, player_setting in GameSettings.players_list]
+
+    if GameSettings.shuffle_players:
+        # dice has a thread-safe copy of random.shuffle
+        dice.shuffle(players)
+
+    # Set up players starting money according to the game settings:
+    # If starting money is a list, assign it to players in order
+    if isinstance(GameSettings.starting_money, list):
+        for player, starting_money in zip(players, GameSettings.starting_money):
+            player.money = starting_money
+    # If starting money is a single value, assign it to all players
+    else:
+        for player in players:
+            player.money = GameSettings.starting_money
+
+    return board, players, dice, log
+
+def test_game(num_turns_to_play):
+    ''' Plays a game for a number of rounds and stop, returns all objects regarding the game'''
+    game_seed = random.random()
+    print(f"seed used: {game_seed}")
+    board, players, dice, log = init_game(game_seed)
+    # Play for the required number of turns
+    for turn_n in range(1, num_turns_to_play):
+        # Players make their moves
+        for player in players:
+            # result will be "bankrupt" if player goes bankrupt
+            result = player.make_a_move(board, players, dice, log)
+            # If player goes bankrupt, log it in the data log file
+    return board, players, dice, log
+
+class Test_Basic_QLearning(unittest.TestCase):
+
+    def test_state_space(self):
+        """Plays a game for 10 rounds, checks if the state space 
+        makes sense for a random player"""
+        _, players, _, _ = test_game(40)
+        current_player = players[0]
+        state = State(current_player, players).state
+        # for player in players:
+        #     print(player.name)
+        #     for property in player.owned:
+        #         print(f"    {property.group} {property.has_houses} {property.has_hotel}")
+        self.assertTrue(np.all((state >= 0) & (state <= 1)))
+        self.assertTrue(len(state) == 23)
+        
+if __name__ == "__main__":
+    unittest.main()
